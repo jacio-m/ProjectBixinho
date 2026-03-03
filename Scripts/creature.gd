acb79ec9fdd4dfadbce5099486d9874854d162eb
Jácio Mauê <jaciomaue@gmail.com>
2026-03-02 21:33:20 -0300

    Added FHD window, Nickname and Health Label for Creatures
    Updated the game window size to 1920x1080, added a variable for a nickname for each new creature and added a health label for the health bar. Also fixed indentation of the if/else logic so that if the creature has 0 health it can't go lower.

--- Scripts/creature.gd
+++ Scripts/creature.gd
@@ -1,59 +1,101 @@
 extends Node
 class_name Creature
 
+@export var nickname = ""
 @export var health = 0
 @export var alive = true
 @export var type = ""
 @export var type2 = ""
 @export var health_bar : ProgressBar
+@export var health_bar_num : Label
 
 func _ready():
 	health_bar.max_value = health
 	health_bar.value = health
+	health_bar_num.text = str(health)
 	$Sprite.play("StandPose")
 	$Animation.play("Bounce")
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
-		take_damage("Salty", 10)
+		take_damage("Salty", 100)
 
 func take_damage(damage_type, damage_val):
-	if damage_type == "Salty" and (self.type == "Hot" or self.type == "Citric"):
-		damage_val *= 2
-	elif damage_type == "Salty" and (self.type == "Spicy" or self.type == "Solid"):
-		damage_val *= 0.5
-	elif damage_type == "Salty" and (self.type == "Salty"):
-		damage_val = 0
+	if alive == false:
+		return
+	else:
+		if damage_type == "Salty" and (self.type == "Hot" or self.type == "Citric"):
+			damage_val *= 2
+		elif damage_type == "Salty" and (self.type == "Spicy" or self.type == "Solid"):
+			damage_val *= 0.5
+		elif damage_type == "Salty" and (self.type == "Salty"):
+			damage_val = 0
 
-	if damage_type == "Hot" and (self.type == "Fruit" or self.type == "Cold"):
-		damage_val *= 2
-	elif damage_type == "Hot" and (self.type == "Liquid" or self.type == "Salty"):
-		damage_val *= 0.5
-	elif damage_type == "Hot" and (self.type == "Hot"):
-		damage_val = 0
-	
-	if damage_type == "Liquid" and (self.type == "Hot" or self.type == "Solid"):
-		damage_val *= 2
-	elif damage_type == "Liquid" and (self.type == "Fruit" or self.type == "Citric"):
-		damage_val *= 0.5
-	elif damage_type == "Liquid" and (self.type == "Liquid"):
-		damage_val = 0
-	
-	if damage_type == "Fruit" and (self.type == "Liquid" or self.type == "Sweet"):
-		damage_val *= 2
-	elif damage_type == "Fruit" and (self.type == "Hot" or self.type == "Cold"):
-		damage_val *= 0.5
-	elif damage_type == "Fruit" and (self.type == "Fruit"):
-		damage_val = 0
-	
-	if damage_type == "Salty" and (self.type == "Hot" or self.type == "Citric"):
-		damage_val *= 2
-	elif damage_type == "Salty" and (self.type == "Spicy" or self.type == "Solid"):
-		damage_val *= 0.5
-	elif damage_type == "Salty" and (self.type == "Salty"):
-		damage_val = 0
-	
-	health -= damage_val
-	health_bar.value = health
-	if health < 0:
+		if damage_type == "Hot" and (self.type == "Fruit" or self.type == "Cold"):
+			damage_val *= 2
+		elif damage_type == "Hot" and (self.type == "Liquid" or self.type == "Salty"):
+			damage_val *= 0.5
+		elif damage_type == "Hot" and (self.type == "Hot"):
+			damage_val = 0
+		
+		if damage_type == "Liquid" and (self.type == "Hot" or self.type == "Solid"):
+			damage_val *= 2
+		elif damage_type == "Liquid" and (self.type == "Fruit" or self.type == "Citric"):
+			damage_val *= 0.5
+		elif damage_type == "Liquid" and (self.type == "Liquid"):
+			damage_val = 0
+		
+		if damage_type == "Fruit" and (self.type == "Liquid" or self.type == "Sweet"):
+			damage_val *= 2
+		elif damage_type == "Fruit" and (self.type == "Hot" or self.type == "Cold"):
+			damage_val *= 0.5
+		elif damage_type == "Fruit" and (self.type == "Fruit"):
+			damage_val = 0
+		
+		if damage_type == "Citric" and (self.type == "Liquid" or self.type == "Spicy"):
+			damage_val *= 2
+		elif damage_type == "Citric" and (self.type == "Salty" or self.type == "Sweet"):
+			damage_val *= 0.5
+		elif damage_type == "Citric" and (self.type == "Citric"):
+			damage_val = 0
+		
+		if damage_type == "Cold" and (self.type == "Fruit" or self.type == "Solid"):
+			damage_val *= 2
+		elif damage_type == "Cold" and (self.type == "Hot" or self.type == "Spicy"):
+			damage_val *= 0.5
+		elif damage_type == "Cold" and (self.type == "Cold"):
+			damage_val = 0
+			
+		if damage_type == "Spicy" and (self.type == "Salty" or self.type == "Cold"):
+			damage_val *= 2
+		elif damage_type == "Spicy" and (self.type == "Citric" or self.type == "Bitter"):
+			damage_val *= 0.5
+		elif damage_type == "Spicy" and (self.type == "Spicy"):
+			damage_val = 0
+		
+		if damage_type == "Bitter" and (self.type == "Spicy" or self.type == "Sweet"):
+			damage_val *= 2
+		elif damage_type == "Bitter" and (self.type == "Solid"):
+			damage_val *= 0.5
+		elif damage_type == "Bitter" and (self.type == "Bitter"):
+			damage_val = 0
+		
+		if damage_type == "Sweet" and (self.type == "Citric" or self.type == "Bitter"):
+			damage_val *= 2
+		elif damage_type == "Sweet" and (self.type == "Fruit"):
+			damage_val *= 0.5
+		elif damage_type == "Sweet" and (self.type == "Sweet"):
+			damage_val = 0
+			
+		if damage_type == "Solid" and (self.type == "Salty" or self.type == "Bitter"):
+			damage_val *= 2
+		elif damage_type == "Solid" and (self.type == "Liquid" or self.type == "Cold"):
+			damage_val *= 0.5
+		elif damage_type == "Solid" and (self.type == "Solid"):
+			damage_val = 0
+			
+		health -= damage_val
+		health_bar.value = health
+		health_bar_num.text = str(health)
+	if health <= 0:
 		alive = false
